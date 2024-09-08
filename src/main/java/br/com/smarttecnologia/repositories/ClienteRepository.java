d10f0683c804837759b0c57e78a93ac598a87415
pedrohamaranhao13 <pedrinhoelloco@outlook.com.br>
2024-09-08 20:15:15 -0300

    feature: Método create no repository

--- src/main/java/br/com/smarttecnologia/repositories/ClienteRepository.java
+++ src/main/java/br/com/smarttecnologia/repositories/ClienteRepository.java
@@ -1,13 +1,30 @@
 package br.com.smarttecnologia.repositories;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 import br.com.smarttecnologia.entities.Cliente;
+import br.com.smarttecnologia.factories.ConnectionFactory;
 
 public class ClienteRepository {
 
 	public void create(Cliente cliente) throws Exception {
-		//  TODO
+
+		Connection connection = ConnectionFactory.getConnection();
+		
+		String sql = "insert into cliente(nome, email, cpf, telefone, observacoes) values(?,?,?,?,?)";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, cliente.getNome());
+		statement.setString(2, cliente.getEmail());
+		statement.setString(3, cliente.getCpf());
+		statement.setString(4, cliente.getTelefone());
+		statement.setString(5, cliente.getObservacoes());
+		statement.execute();
+		
+		connection.close();
+		
 	}
 	
 	public void update(Cliente cliente) throws Exception {
